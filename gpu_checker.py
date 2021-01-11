1feded73ed0fedb229cff6ca23784ae6977f7616
xusheng <1500012780@pku.edu.cn>
2021-01-11 14:31:10 +0800

    gpu blacklist

--- gpu_checker.py
+++ gpu_checker.py
@@ -9,7 +9,7 @@ import colorama
 target_memory = 1000
 
 cluster_blacklist = [2]
-# gpu_blacklist = [(7, 'GPU0')]
+gpu_blacklist = [(7, 'GPU0')]
 
 r = requests.get("http://172.31.32.100/gpustat/info.js")
 text = r.text
@@ -21,5 +21,13 @@ for i in range(10):
         continue
     cluster_key = 'icst' + str(i)
     for gpu_key, gpu_value in state_dict[cluster_key].items():
+        black_flag = False
+        for black_i, black_gpu in gpu_blacklist:
+            if i == black_i and gpu_key == black_gpu:
+                black_flag = True
+                break
+        if black_flag:
+            continue
+
         if gpu_value['memory.used'] <= target_memory:
             print(f"{cluster_key} | GPU{gpu_key} | 使用显存 {gpu_value['memory.used']: 5} | 使用率 {gpu_value['utilization.gpu']:3} | 温度 {gpu_value['temperature.gpu']}")
