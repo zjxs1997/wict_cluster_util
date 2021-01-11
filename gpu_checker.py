34f2b588eff94bad1c594c83a0f4b8e9e59be4ff
xusheng <1500012780@pku.edu.cn>
2021-01-11 10:15:23 +0800

    first commit

---
+++ gpu_checker.py
@@ -0,0 +1,24 @@
+# todo: add gpu_blacklist support
+# todo: colorful display
+
+import requests
+import json
+import colorama
+
+target_memory = 1000
+
+cluster_blacklist = [2]
+# gpu_blacklist = [(7, 'GPU0')]
+
+r = requests.get("http://172.31.32.100/gpustat/info.js")
+text = r.text
+json_text = text[6:-2]
+state_dict = json.loads(json_text)
+
+for i in range(10):
+    if i in cluster_blacklist:
+        continue
+    cluster_key = 'icst' + str(i)
+    for gpu_key, gpu_value in state_dict[cluster_key].items():
+        if gpu_value['memory.used'] <= target_memory:
+            print(f"{cluster_key} | GPU{gpu_key} | 使用显存 {gpu_value['memory.used']: 5} | 使用率 {gpu_value['utilization.gpu']:3} | 温度 {gpu_value['temperature.gpu']}")
