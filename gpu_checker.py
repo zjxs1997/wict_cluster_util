522266a89c89bbb44a745daefc1a57c8f2ed2676
zjxs1997 <1500012780@pku.edu.cn>
2021-01-11 21:23:37 +0800

    miner change

--- gpu_checker.py
+++ gpu_checker.py
@@ -1,6 +1,6 @@
-# todo: add gpu_blacklist support
 # todo: colorful display
-# miner change to test github push
+# todo: add wechat notification
+# todo: use a configuration file
 
 import requests
 import json
@@ -11,10 +11,17 @@ target_memory = 1000
 cluster_blacklist = [2]
 gpu_blacklist = [(7, 'GPU0')]
 
+use_wechat_notify = False
+wechat_schkey = 'xxx'
+notify_every_seconds = 3600*24
+# 可以把上次通知时间写到本地某个文件里，每次读取，比较是否经过那么多时间
+
 r = requests.get("http://172.31.32.100/gpustat/info.js")
 text = r.text
 json_text = text[6:-2]
 state_dict = json.loads(json_text)
+
+texts = []
 
 for i in range(10):
     if i in cluster_blacklist:
@@ -30,4 +37,6 @@ for i in range(10):
             continue
 
         if gpu_value['memory.used'] <= target_memory:
-            print(f"{cluster_key} | GPU{gpu_key} | 使用显存 {gpu_value['memory.used']: 5} | 使用率 {gpu_value['utilization.gpu']:3} | 温度 {gpu_value['temperature.gpu']}")
+            text = f"{cluster_key} | GPU{gpu_key} | 使用显存 {gpu_value['memory.used']: 5} | 使用率 {gpu_value['utilization.gpu']:3} | 温度 {gpu_value['temperature.gpu']}"
+            print(text)
+            texts.append(text)
